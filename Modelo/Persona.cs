5e99ca54f5544dd992cf0b94a2c045be3a2bc0da
DomiDelarosa <marengodelarosa2@gmail.com>
2026-05-18 12:29:42 -0500

    Agregar clase Persona.cs

---
+++ Modelo/Persona.cs
@@ -0,0 +1,21 @@
+namespace Modelo
+{
+   public class Persona
+   {
+      public string Nombre { get; set; }
+      public string Apellido { get; set; }
+      public int Edad { get; set; }
+      public string Email { get; set; }
+      public string Telefono { get; set; }
+
+      protected Persona(string nombre, string apellido, string email, string telefono)
+      {
+         Nombre = nombre;
+         Apellido = apellido;
+         Email = email;
+         Telefono = telefono;
+      }
+
+      public string GetNombreCompleto() => $"{Nombre} {Apellido}";
+   }
+}
